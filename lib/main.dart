1914adf335acce49f23128ccbb52186c78532690
Yaksha69 <minato18q@gmail.com>
2025-06-19 10:07:18 +0800

    text alignments

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,84 @@ void main() {
         title: Text('Recipe App'),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Text('gitnang text are', style: TextStyle(
-              fontSize: 24,
-              foreground: Paint()
+      body: Column(
+        children:[
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Name',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                                  foreground: Paint()
                 ..shader = LinearGradient(
                   colors: [
                     Colors.red, // dark gray
                     Colors.green, // medium gray
                     Colors.blue, // light gray
                   ],
-                ).createShader(Rect.fromLTWH(100.0, 10.0, 200.0, 70.0)),
-              fontWeight: FontWeight.bold,
-              decorationColor: Colors.red,
-              decoration: TextDecoration.underline,
-              decorationStyle: TextDecorationStyle.wavy,
-                
-            ))
-          ],
-        )
+                ).createShader(Rect.fromLTWH(5.0, 0.0, 200.0, 70.0)),
+                    
+                  ),
+                ),
+                Container(
+                  color: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Text(
+                    'duon'
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              children: [
+                Text(
+                  'Age',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue
+                  ),
+                ),
+                Container(
+                  color: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Text(
+                    'sa gitna'
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB( 1, 2, 3, 4),
+            child: Row(
+              children: [
+                Text(
+                  'Gender',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                  color: Colors.lightBlueAccent,
+                  child: Text(
+                    'sa ilalim'
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
       )
     ),
