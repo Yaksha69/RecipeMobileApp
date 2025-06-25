3852ab935f16802a34acc7a63716e9fc0b80d1c2
Yaksha69 <minato18q@gmail.com>
2025-06-25 09:49:35 +0800

    UserProfileLayout

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,180 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-    home: Scaffold(
+  runApp(
+    MaterialApp(
+      home: Profile(),
+    ),
+  );
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Recipe App'),
         backgroundColor: Colors.green,
       ),
       body: Column(
-        children:[
+        children: [
           Container(
-            margin: EdgeInsets.all(10),
+            color: Colors.yellow,
+            margin: EdgeInsets.only(top: 20, bottom: 20),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'User Profile',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Icon(
+                  Icons.android_outlined,
+                  size: 100,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(top: 10, left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   'Name',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
-                                  foreground: Paint()
-                ..shader = LinearGradient(
-                  colors: [
-                    Colors.red, // dark gray
-                    Colors.green, // medium gray
-                    Colors.blue, // light gray
-                  ],
-                ).createShader(Rect.fromLTWH(5.0, 0.0, 200.0, 70.0)),
-                    
+                    color: Colors.blue,
                   ),
                 ),
-                Container(
-                  color: Colors.red,
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  child: Text(
-                    'duon'
+                Text(
+                  'Terdy',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
                   ),
-                )
+                ),
               ],
             ),
           ),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            child: Row(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(top: 10, left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Age',
+                  'Email',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
-                    color: Colors.blue
+                    color: Colors.blue,
                   ),
                 ),
-                Container(
-                  color: Colors.red,
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  child: Text(
-                    'sa gitna'
+                Text(
+                  'terdy@g.com',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
                   ),
-                )
+                ),
               ],
             ),
           ),
           Container(
-            margin: EdgeInsets.fromLTRB( 1, 2, 3, 4),
-            child: Row(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(top: 10, left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Gender',
+                  'Company',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
-                    color: Colors.red
+                    color: Colors.blue,
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-                  color: Colors.lightBlueAccent,
-                  child: Text(
-                    'sa ilalim'
+                Text(
+                  'QWERTYUI.company',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
                   ),
-                )
+                ),
               ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(top: 10, left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Contact Number',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+                Text(
+                  '1234567890',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 100, bottom: 20),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                iconColor: Colors.green,
+                padding: EdgeInsets.all(20),
+                fixedSize: Size(300, 50),
+              ),
+              onPressed: (){},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.edit, size: 20),
+                  SizedBox(width: 5),
+                  Text(
+                    'Edit Profile',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
-      )
-    ),
-  );
+    );
+  }
 }
-
